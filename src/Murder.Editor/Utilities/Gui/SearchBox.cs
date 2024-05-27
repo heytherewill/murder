f27e0480f594dc5c855181a035911f0e716803f3
William Barbosa <heytherewill@gmail.com>
2024-05-27 20:57:09 +0200

    🖥️ Render editor fields

--- src/Murder.Editor/Utilities/Gui/SearchBox.cs
+++ src/Murder.Editor/Utilities/Gui/SearchBox.cs
@@ -6,6 +6,7 @@ using Murder.Attributes;
 using Murder.Core.Dialogs;
 using Murder.Core.Geometry;
 using Murder.Core.Sounds;
+using Murder.Core.Ui;
 using Murder.Editor.Utilities;
 using Murder.Prefabs;
 using Murder.Utilities;
@@ -666,6 +667,19 @@ namespace Murder.Editor.ImGuiExtended
                 SearchFrameSize: new Vector2(150, 200),
                 SearchBoxContainerSize: new Vector2(250, 400)
             );
+        }
+
+        public static Type? SearchConstraints()
+        {
+            string selected = "Select a constraint";
+
+            // Find all non-repeating components
+            IEnumerable<Type> types = ReflectionHelper.SafeGetAllTypesInAllAssemblies()
+                .Where(p => !p.IsInterface && typeof(IConstraint).IsAssignableFrom(p));
+
+            Lazy<Dictionary<string, Type>> candidates = new(CollectionHelper.ToStringDictionary(types, t => t.Name, t => t));
+
+            return Search(id: "constraint_search", hasInitialValue: false, selected, values: candidates, SearchBoxFlags.None, out Type? chosen) ? chosen : default;
         }
     }
 }
